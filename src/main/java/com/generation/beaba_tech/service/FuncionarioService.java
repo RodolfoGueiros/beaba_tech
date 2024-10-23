04f9e69b3dd35b0e88aa3cbd6c5b16bd6c53912f
NicXavier <notedenic@gmail.com>
2024-10-23 17:35:48 -0300

    Acrescentado a autenticação de funcionário

--- src/main/java/com/generation/beaba_tech/service/FuncionarioService.java
+++ src/main/java/com/generation/beaba_tech/service/FuncionarioService.java
@@ -1,18 +1,32 @@
 package com.generation.beaba_tech.service;
 
+import com.generation.beaba_tech.dto.FuncionarioLogin;
 import com.generation.beaba_tech.entity.Funcionario;
 import com.generation.beaba_tech.repository.FuncionarioRepository;
 import com.generation.beaba_tech.exception.ResourceNotFoundException;
 import com.generation.beaba_tech.exception.BusinessException;
+import com.generation.beaba_tech.security.JwtService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class FuncionarioService {
+    @Autowired
+    private JwtService jwtService;
+    
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
     private final FuncionarioRepository funcionarioRepository;
     private final PasswordEncoder passwordEncoder;
 
@@ -54,5 +68,39 @@ public class FuncionarioService {
     public void delete(Long id) {
         Funcionario funcionario = findById(id);
         funcionarioRepository.delete(funcionario);
+    }
+
+    public Optional<FuncionarioLogin> authenticateUser(Optional<FuncionarioLogin> funcionarioLogin) {
+        if (funcionarioLogin.isEmpty()) {
+            return Optional.empty();
+        }
+        
+        var credentials = new UsernamePasswordAuthenticationToken(funcionarioLogin.get().getEmail(), funcionarioLogin.get().getSenha());
+        
+        Authentication authentication = authenticationManager.authenticate(credentials);
+        
+        if (authentication.isAuthenticated()) {
+            Optional<Funcionario> user = funcionarioRepository.findByEmail(funcionarioLogin.get().getEmail());
+            
+            if (user.isPresent()) {
+                funcionarioLogin.get().setId(user.get().getId());
+                funcionarioLogin.get().setNome(user.get().getNome());
+                funcionarioLogin.get().setEmail(user.get().getEmail());
+                funcionarioLogin.get().setSenha("");
+                funcionarioLogin.get().setToken(generateToken(funcionarioLogin.get().getEmail()));
+                
+                return funcionarioLogin;
+            }
+        }
+        return Optional.empty();
+    }
+    
+    private String hashPassword(String password) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        return encoder.encode(password);
+    }
+    
+    private String generateToken(String user) {
+        return "Bearer " + jwtService.generateToken(user);
     }
 }
