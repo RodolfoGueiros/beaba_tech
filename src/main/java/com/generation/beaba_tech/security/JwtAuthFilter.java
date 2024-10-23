05621f8473a6d47f7b70e5b95b1ab75746c1e698
NicXavier <notedenic@gmail.com>
2024-10-23 17:53:31 -0300

    Camada security: JwtAuthFilter

--- src/main/java/com/generation/beaba_tech/security/JwtAuthFilter.java
+++ src/main/java/com/generation/beaba_tech/security/JwtAuthFilter.java
@@ -20,42 +20,42 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.io.IOException;
 import java.security.SignatureException;
+
 @Component
 public class JwtAuthFilter extends OncePerRequestFilter {
   
-    @Autowired
-    private JwtService jwtService;
+  @Autowired
+  private JwtService jwtService;
+  
+  @Autowired
+  UserDetailsServiceImpl userDetailsService;
+  
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull FilterChain filterChain) throws ServletException, IOException {
+    String authHeader = request.getHeader("Authorization");
+    String token = null;
+    String username = null;
     
-    @Autowired
-    UserDetailsServiceImpl userDetailsService;
-    
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull FilterChain filterChain) throws ServletException, IOException {
-      String authHeader = request.getHeader("Authorization");
-      String token = null;
-      String username = null;
-      
-      try {
-        if(authHeader != null && authHeader.startsWith("Bearer ")){
-          token = authHeader.substring(7);
-          username = jwtService.extractUsername(token);
-        }
-        
-        if(username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-          UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-          
-          if (jwtService.validateToken(token, userDetails)) {
-            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-            authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-            SecurityContextHolder.getContext().setAuthentication(authToken);
-          }
-        }
-        filterChain.doFilter(request, response);
-      } catch(ExpiredJwtException | UnsupportedJwtException | MalformedJwtException
-              | ResponseStatusException e) {
-        response.setStatus(HttpStatus.FORBIDDEN.value());
-        return;
+    try {
+      if(authHeader != null && authHeader.startsWith("Bearer ")){
+        token = authHeader.substring(7);
+        username = jwtService.extractUsername(token);
       }
+      
+      if(username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        
+        if (jwtService.validateToken(token, userDetails)) {
+          UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+          authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+          SecurityContextHolder.getContext().setAuthentication(authToken);
+        }
+      }
+      filterChain.doFilter(request, response);
+    } catch(ExpiredJwtException | UnsupportedJwtException | MalformedJwtException
+            | ResponseStatusException e) {
+      response.setStatus(HttpStatus.FORBIDDEN.value());
+      return;
     }
   }
-  
+}
